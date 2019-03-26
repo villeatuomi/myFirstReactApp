444f7cf2276d82d82f754cdcdbb3a2a6f2008d87
villeatuomi <vat92@hotmail.com>
2019-03-26 15:14:46 +0200

    added dot to urls in Nav.js and Tr.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,10 +6,10 @@ const Nav = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="./">Home</Link>
           </li>
           <li>
-            <Link to='/profile'>Profile</Link>
+            <Link to='./profile'>Profile</Link>
           </li>
         </ul>
       </nav>
